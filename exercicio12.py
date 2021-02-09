742d82bbf4611b0172e21fc4826440f2cc6254ae
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-09 15:11:58 -0300

    Update exercicio12.py

--- exercicio12.py
+++ exercicio12.py
@@ -6,11 +6,12 @@ conexao = sqlite3.connect('cadastro.db')
 # criar um curso para manipulação dos dados e tabelas
 cursor = conexao.cursor()
 
+# deleta a linha que possui o nome "cliente 7"
 cursor.execute("DELETE from cliente WHERE nome = 'cliente 7'")
 
+# executa comando select para recuperar as informações da tabela
 for linha in cursor.execute("select * from cliente"):
     print(linha)
-
 
 # confirmar a operação
 conexao.commit()
