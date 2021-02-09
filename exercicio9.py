226ca3358f18f1329a7f0c4223c7410ac48a7c60
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-09 15:08:35 -0300

    Update exercicio9.py

--- exercicio9.py
+++ exercicio9.py
@@ -6,8 +6,10 @@ conexao = sqlite3.connect('cadastro.db')
 # criar um curso para manipulação dos dados e tabelas
 cursor = conexao.cursor()
 
+# altera todos os valores da coluna estado para "SC"
 cursor.execute("UPDATE cliente SET estado = 'SC'")
 
+# executar comando select para recuperar as informações da tabela
 for linha in cursor.execute("select * from cliente"):
     print(linha)
     
