31ffcf23fa21d48504f936b843c7a26ecde1b9aa
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-09 15:09:44 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -6,9 +6,11 @@ conexao = sqlite3.connect('cadastro.db')
 # criar um curso para manipulação dos dados e tabelas
 cursor = conexao.cursor()
 
+# altera os dados da coluna celular que são iguais a variavel "filtro"
 filtro = '22222-2222'
 cursor.execute("UPDATE cliente SET celular = '92323-9987' WHERE celular like ?", (filtro + "%",))
 
+# executar comando select para recuperar as informações da tabela
 for linha in cursor.execute("select * from cliente"):
     print(linha)
 
