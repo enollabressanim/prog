f6d70a6a70011768efe8a30098bd24bfb76d6b18
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-09 15:12:56 -0300

    Update exercicio13.py

--- exercicio13.py
+++ exercicio13.py
@@ -6,8 +6,10 @@ conexao = sqlite3.connect('cadastro.db')
 # criar um curso para manipulação dos dados e tabelas
 cursor = conexao.cursor()
 
+# adiciona uma nova coluna na tabela "cliente"
 cursor.execute('ALTER TABLE cliente ADD COLUMN idade TEXT;')
 
+# executa comando select para recuperar as informações da tabela
 for linha in cursor.execute("select * from cliente"):
     print(linha)
 
