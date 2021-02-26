47651fe752776ce8a2f4d423e8c92012237f1002
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-26 13:46:06 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,5 +1,10 @@
 import sqlite3
 
+# O exercício 5 pede para listar todas as linhas e colunas armazenadas 
+# no banco de dados ordenadas por alguma coluna de escolha particular.
+# Para que isso aconteça, é selecionado todos os dados da tabela cliente
+# e assim sendo ordenada pelo e-mail.
+
 # criar uma "conexão" com o banco de dados
 conexao = sqlite3.connect('cadastro.db')
 
