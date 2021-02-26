7e9cb7de1d925f8f5ec2a23faae54a9f14bd8c1f
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-26 12:13:33 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,5 +1,14 @@
 import sqlite3
 
+# O exercício 10 pede para que seja atualizado determinadas linhas de uma coluna da tabela.
+# Para que isso ocorra, é utilizado o UPDATE, atualizando os dados da tabela cliente, 
+# selecionando a coluna celular, para que a mesma seja atualizada.
+# Para que aja a atualização de apenas determinadas linhas, é necessario o uso do where, 
+# que tem como finalidade modificar apenas a linha que possui um valor já determinado, 
+# no caso, nesta situação, mudando apenas a coluna celular que possui o valor 22222-2222.
+# Usando o operador LIKE combinado com o caracter especial porcentagem (%),
+# há a indicação da posição que o conteúdo está sendo procurado no valor string do campo especificado.
+
 # criar uma "conexão" com o banco de dados
 conexao = sqlite3.connect('cadastro.db')
 
