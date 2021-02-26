5e850a5aad37a4dcb867569c3c1c7367e3e0a75f
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-26 11:50:02 -0300

    Update exercicio9.py

--- exercicio9.py
+++ exercicio9.py
@@ -1,5 +1,10 @@
 import sqlite3
 
+# O exercício 9 pede para que seja atualizado todas as linhas de uma coluna da tabela.
+# Para que isso ocorra, é utilizado o UPDATE, atualizando os dados da tabela cliente, 
+# onde irá selecionar a coluna estado, mudando todas as suas linhas para SC
+
+
 # criar uma "conexão" com o banco de dados
 conexao = sqlite3.connect('cadastro.db')
 
