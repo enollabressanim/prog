d5f7aef7f160a8fd3f5c404559fb4606962d6280
Enolla Cristine Bressanim <49999110+enollabressanim@users.noreply.github.com>
2021-02-26 12:20:23 -0300

    Update exercicio12.py

--- exercicio12.py
+++ exercicio12.py
@@ -1,5 +1,12 @@
 import sqlite3
 
+# O exercício 12 pede para que  faça a exclusão de uma linha específica da tabela.
+# Para que isso ocorra, é utilizado o UPDATE, atualizando os dados da tabela cliente, 
+# selecionando a coluna celular, para que a mesma seja atualizada.
+# Para que aja a atualização de apenas determinadas linhas, é necessario o uso do where, 
+# que tem como finalidade modificar apenas a linha que possui um valor já determinado, 
+# no caso, nesta situação, mudando apenas a coluna nome, que tem como valor Cliente 7.
+
 # criar uma "conexão" com o banco de dados
 conexao = sqlite3.connect('cadastro.db')
 
